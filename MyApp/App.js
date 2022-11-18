72548359f211eeb76436a491b3b4b4e4a29b56a3
cesarhro <cesar.olimpio@zrobank.com.br>
2022-11-17 22:03:16 -0300

    First commit

---
+++ MyApp/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>César</Text>
+      <Image
+        source={require("./assets/github.jpg")}
+        style={{ width: 300, height: 300 }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
